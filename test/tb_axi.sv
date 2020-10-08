80868e322a1b92246dab221055d3f8c053580ba1
Azhar Madar Shaik <azarshaik454@gmail.com>
2020-10-08 15:21:35 +0530

    indents

--- test/tb_axi.sv
+++ test/tb_axi.sv
@@ -4,69 +4,69 @@ import axi_pkg::*;
 
 module tb_axi;
 
-	localparam STEP = 10;
-	
-	logic aclk, areset_n;
-	axi_if axi_if();
-	logic start_read;
-	logic start_write;
+  localparam STEP = 10;
 
-	axi_master master(
-		.aclk(aclk), .areset_n(areset_n),
-		.m_axi(axi_if),
-		.start_read(start_read), .start_write(start_write)
-	);
+  logic aclk, areset_n;
+  axi_if axi_if();
+  logic start_read;
+  logic start_write;
 
-	axi_slave slave(
-		.aclk(aclk), .areset_n(areset_n),
-		.s_axi(axi_if)
-	);
+  axi_master master(
+    .aclk(aclk), .areset_n(areset_n),
+    .m_axi(axi_if),
+    .start_read(start_read), .start_write(start_write)
+  );
 
-	always begin
-		aclk = 1; #(STEP / 2);
-		aclk = 0; #(STEP / 2);
-	end
+  axi_slave slave(
+    .aclk(aclk), .areset_n(areset_n),
+    .s_axi(axi_if)
+  );
 
-	initial begin
-		start_read = 0; start_write = 0;
-		areset_n = 1;
-		#(STEP * 10) areset_n = 0;
-		#(STEP * 10) areset_n = 1;
-		start_write = 1; #(STEP) start_write = 0;
-		#(STEP * 10)
-		start_read = 1; #(STEP) start_read = 0;
+  always_ff begin
+    aclk = 1; #(STEP / 2);
+    aclk = 0; #(STEP / 2);
+  end
 
-		#(STEP * 10);
+  initial begin
+    start_read = 0; start_write = 0;
+    areset_n = 1;
+    #(STEP * 10) areset_n = 0;
+    #(STEP * 10) areset_n = 1;
+    start_write = 1; #(STEP) start_write = 0;
+    #(STEP * 10)
+    start_read = 1; #(STEP) start_read = 0;
 
-		test_write();
-		test_read();
+    #(STEP * 10);
 
-		$finish;
-	end
+    test_write();
+    test_read();
 
-	addr_t addr = 32'h4;
-	data_t data = 32'hdeadbeef;
-	int flag_w = 1, flag_r = 1;
-	
-	task test_write;
-		for (int i = 0; i < 4; i++) begin
-			if (slave.buffer[addr + i] != data + i) flag_w = 0;
-			$display("actual:%h expected:%h\n", slave.buffer[addr + i], data + i);
-		end
+    $finish;
+  end
 
-		if (flag_w) $display("Pass");
-		else $display("Fail");
-	endtask : test_write
+  addr_t addr = 32'h4;
+  data_t data = 32'hdeadbeef;
+  int flag_w = 1, flag_r = 1;
 
-	task test_read;
-		for (int i = 0; i < 4; i++) begin
-			if (master.rdata[addr + i] != data + i) flag_r = 0;
-			$display("actual:%h expected:%h\n", master.rdata[addr + i], data + i);
-		end
+  task test_write;
+    for (int i = 0; i < 4; i++) begin
+      if (slave.buffer[addr + i] != data + i) flag_w = 0;
+      $display("actual:%h expected:%h\n", slave.buffer[addr + i], data + i);
+    end
 
-		if (flag_r) $display("Pass");
-		else $display("Fail");
-	endtask : test_read
-	
-	
+    if (flag_w) $display("Pass");
+    else $display("Fail");
+  endtask : test_write
+
+  task test_read;
+    for (int i = 0; i < 4; i++) begin
+      if (master.rdata[addr + i] != data + i) flag_r = 0;
+      $display("actual:%h expected:%h\n", master.rdata[addr + i], data + i);
+    end
+
+    if (flag_r) $display("Pass");
+    else $display("Fail");
+  endtask : test_read
+
+
 endmodule
--- test/tb_axi_lite_interconnect.sv
+++ test/tb_axi_lite_interconnect.sv
@@ -4,89 +4,89 @@ import axi_lite_pkg::*;
 
 module tb_axi_lite_interconnect;
 
-	localparam STEP = 10;
-	localparam addr0 = 32'h4;
-	localparam addr1 = 32'h14;
+localparam STEP = 10;
+localparam addr0 = 32'h4;
+localparam addr1 = 32'h14;
 
-	logic aclk, areset_n;
-	logic start_read_0, start_read_1;
-	logic start_write_0, start_write_1;
+logic aclk, areset_n;
+logic start_read_0, start_read_1;
+logic start_write_0, start_write_1;
 
-	axi_lite_if axi_lite_if_m0();
-	axi_lite_if axi_lite_if_m1();
-	axi_lite_if axi_lite_if_s0();
-	axi_lite_if axi_lite_if_s1();
+axi_lite_if axi_lite_if_m0();
+axi_lite_if axi_lite_if_m1();
+axi_lite_if axi_lite_if_s0();
+axi_lite_if axi_lite_if_s1();
 
-	axi_lite_master #(addr0) master0 (
-		.aclk(aclk), .areset_n(areset_n),
-		.m_axi_lite(axi_lite_if_m0),
-		.start_read(start_read_0), .start_write(start_write_0)
-	);
+axi_lite_master #(addr0) master0 (
+  .aclk(aclk), .areset_n(areset_n),
+  .m_axi_lite(axi_lite_if_m0),
+  .start_read(start_read_0), .start_write(start_write_0)
+);
 
-	axi_lite_master #(addr1) master1 (
-		.aclk(aclk), .areset_n(areset_n),
-		.m_axi_lite(axi_lite_if_m1),
-		.start_read(start_read_1), .start_write(start_write_1)
-	);
+axi_lite_master #(addr1) master1 (
+  .aclk(aclk), .areset_n(areset_n),
+  .m_axi_lite(axi_lite_if_m1),
+  .start_read(start_read_1), .start_write(start_write_1)
+);
 
-	axi_lite_slave slave0 (
-		.aclk(aclk), .areset_n(areset_n),
-		.s_axi_lite(axi_lite_if_s0)
-	);
+axi_lite_slave slave0 (
+  .aclk(aclk), .areset_n(areset_n),
+  .s_axi_lite(axi_lite_if_s0)
+);
 
-	axi_lite_slave slave1 (
-		.aclk(aclk), .areset_n(areset_n),
-		.s_axi_lite(axi_lite_if_s1)
-	);
+axi_lite_slave slave1 (
+  .aclk(aclk), .areset_n(areset_n),
+  .s_axi_lite(axi_lite_if_s1)
+);
 
-	axi_lite_interconnect axi_lite_ic (
-		.aclk(aclk), .areset_n(areset_n),
-		.axim('{axi_lite_if_m0, axi_lite_if_m1}), .axis('{axi_lite_if_s0, axi_lite_if_s1})
-	);
+axi_lite_interconnect axi_lite_ic (
+  .aclk(aclk), .areset_n(areset_n),
+  .axim('{axi_lite_if_m0, axi_lite_if_m1}), .axis('{axi_lite_if_s0, axi_lite_if_s1})
+);
 
-	always begin
-		aclk = 1; #(STEP / 2);
-		aclk = 0; #(STEP / 2);
-	end
+always begin
+  aclk = 1; #(STEP / 2);
+  aclk = 0; #(STEP / 2);
+end
 
-	initial begin
-		start_read_0 = 0; start_write_0 = 0;
-		start_read_1 = 0; start_write_1 = 0;
-		areset_n = 1;
-		#(STEP * 10) areset_n = 0;
-		#(STEP * 10) areset_n = 1;
-		start_write_0 = 1; start_write_1 = 1; #(STEP) start_write_0 = 0; start_write_1 = 0;
-		#(STEP * 20)
-		start_read_0 = 1; start_read_1 = 1; #(STEP) start_read_0 = 0;
-		#(STEP * 20);
+initial begin
+  start_read_0 = 0; start_write_0 = 0;
+  start_read_1 = 0; start_write_1 = 0;
+  areset_n = 1;
+  #(STEP * 10) areset_n = 0;
+  #(STEP * 10) areset_n = 1;
+  start_write_0 = 1; start_write_1 = 1; #(STEP) start_write_0 = 0; start_write_1 = 0;
+  #(STEP * 20)
+  start_read_0 = 1; start_read_1 = 1; #(STEP) start_read_0 = 0;
+  #(STEP * 20);
 
-		test_write();
-		test_read();
+  test_write();
+  test_read();
 
-		$finish;
-	end
+  $finish;
+end
 
-	data_t data = 32'hdeadbeef;
-	int flag_w = 1, flag_r = 1;
-	
-	task test_write();
-		if (slave0.buffer[addr0] != data) flag_w = 0;
-		$display("actual:%h expected:%h\n", slave0.buffer[addr0], data);
-		if (slave1.buffer[addr1] != data) flag_w = 0;
-		$display("actual:%h expected:%h\n", slave1.buffer[addr1], data);
+data_t data = 32'hdeadbeef;
+int flag_w = 1, flag_r = 1;
 
-		if (flag_w) $display("Pass");
-		else $display("Fail");
-	endtask : test_write
+task test_write();
+  if (slave0.buffer[addr0] != data) flag_w = 0;
+  $display("actual:%h expected:%h\n", slave0.buffer[addr0], data);
+if (slave1.buffer[addr1] != data) flag_w = 0;
+$display("actual:%h expected:%h\n", slave1.buffer[addr1], data);
 
-	task test_read();
-		if (master0.rdata != data) flag_r = 0;
-		$display("actual:%h expected:%h\n", master0.rdata, data);
-		if (master1.rdata != data) flag_r = 0;
-		$display("actual:%h expected:%h\n", master1.rdata, data);
+                if (flag_w) $display("Pass");
+                else $display("Fail");
+              endtask : test_write
 
-		if (flag_r) $display("Pass");
-		else $display("Fail");
-	endtask : test_read
+            task test_read();
+              if (master0.rdata != data) flag_r = 0;
+              $display("actual:%h expected:%h\n", master0.rdata, data);
+            if (master1.rdata != data) flag_r = 0;
+            $display("actual:%h expected:%h\n", master1.rdata, data);
 
-endmodule
+          if (flag_r) $display("Pass");
+          else $display("Fail");
+        endtask : test_read
+
+      endmodule
